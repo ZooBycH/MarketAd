3d5965f79342b3bf1fbc68f12039fa76b84a715b
ZooBycH <mz88@list.ru>
2022-12-03 22:26:03 +0300

    Coursework_6 completed

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,38 @@
-from django.conf import settings
+
 from django.db import models
+
+"""
+Модель **объявления** должна содержать следующие поля:
+
+- title — название товара.
+- price — цена товара (целое число).
+- description — описание товара.
+- author — пользователь, который создал объявление.
+- created_at — время и дата создания объявления.
+
+*Все записи при выдаче должны быть отсортированы по дате создания 
+(чем новее, тем выше).*
+
+Модель **отзыва** должна содержать следующие поля:
+
+- text — текст отзыва.
+- author — пользователь, который оставил отзыв.
+- ad — объявление, под которым оставлен отзыв.
+- created_at - время и дата создания отзыва
+"""
 
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=100, unique=True)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='ad_images', null=True, blank=True)
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,9 @@
-# TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    messages = "You are not owner"
+
+    def has_object_permission(self, request, view, obj):
+        return  obj.author == request.user
+
